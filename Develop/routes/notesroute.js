b62d9f8d52a95fef37e8919985698c3cc10f92b2
Av720 <120581801+Av720@users.noreply.github.com>
2023-03-12 01:17:33 -0700

    refactored and cleaned the dependecies code, added starter code for delete function

--- Develop/routes/notesroute.js
+++ Develop/routes/notesroute.js
@@ -1,32 +1,42 @@
-// create the dependecies for express
-const express = require("express"); // import express
-const path = require("path"); //import node package path
-const app = express(); //init express instance
-const data = require('../db/db.json')
-const fs = require('fs')
-const db = require('../db/db.json')
+const fs = require("fs");
+const data = require("../db/db.json");
 
+// exporting the modules function
+module.exports = function (app) {
+  //create a GET request for "http:localhost:3001/api/notes"
+  app.get("/api/notes", (request, response) => {
+    //use the data from db.json
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
-//GET route for /notes.html
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/notes.html'))
-);
+    // send the data to the GET request
+    response.json(data);
+  });
 
-//GET route for /api/notes
-app.get('/api/notes', (req, res) => {
-    res.json(data);
-});
+  // create a POST request for "http:localhost:3001/api/notes"
+  // This POST request will create a note on the side pannel and will be store in the db.json file
 
-//POST route for /api/notes
-app.post('/api/notes', (req, res) => {
-    const newUserNote = req.body
+  app.post("/api/notes", (request, response) => {
+    //create a variable for the user notes
+
+    const userNote = request.body;
+
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+    // push the new notes from the user into the db.json file
+    data.push(userNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(data));
+
+    console.log("\nSuccess! Note added to the db.json file!");
+
+    // This will send the reponse
+    response.json(data);
+  });
+
+  // BONUS: CREATE a DELETE request in order to delete the note using "delete"
+
+    app.delete("/api/notes/:id", (request, response) => {
+
     
-    db.push(newUserNote)
-
-    fs.writeFileSync('./db/db.json', JSON.stringify(db))
-
-    res.json(db)
-
-
-})
-
+    });
+};
