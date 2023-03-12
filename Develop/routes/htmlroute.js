1534195775025e5d9c5e81984151f9628c29e7a6
Av720 <120581801+Av720@users.noreply.github.com>
2023-03-12 01:14:04 -0700

    added html route and get request

---
+++ Develop/routes/htmlroute.js
@@ -0,0 +1,11 @@
+// Dependencies
+const path = require("path");
+
+// Routing
+module.exports = function (app) {
+
+    // create a GET request for the user to see the html file 
+    app.get('/notes', function (request, response) {
+        response.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
+}
